791bdf92a5697261c78b308bc313821d93000d89
AliR <AliR@ALI>
2025-02-22 09:45:56 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./sections/navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      {/* projects */}
+      {/* eperience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
