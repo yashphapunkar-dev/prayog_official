c2973d4940d0ecfce10f567f70e9546eb08bf3c7
AliR <AliR@ALI>
2025-02-23 11:55:28 +0330

    Add Projects to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./sections/navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Projects from "./sections/Projects";
 
 const App = () => {
   return (
@@ -9,11 +10,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
       <section className="min-h-screen" />
       <section className="min-h-screen" />
       <section className="min-h-screen" />
-      <section className="min-h-screen" />
-      {/* projects */}
       {/* eperience */}
       {/* testimonial */}
       {/* contact */}
