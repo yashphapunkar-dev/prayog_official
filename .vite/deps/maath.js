e330e476ee2e86f8a0877f8487257c271d0d1bc2
yashphapunkar <yashphapunkar@gmail.com>
2025-06-29 13:59:48 +0530

    test2

---
+++ .vite/deps/maath.js
@@ -0,0 +1,26 @@
+import {
+  buffer,
+  easing,
+  geometry,
+  index,
+  matrix,
+  misc,
+  three,
+  triangle,
+  vector2,
+  vector3
+} from "./chunk-G66DV4QE.js";
+import "./chunk-JYTD6HHN.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  buffer,
+  easing,
+  geometry,
+  matrix,
+  misc,
+  index as random,
+  three,
+  triangle,
+  vector2,
+  vector3
+};
